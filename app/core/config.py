c5d9f8c181265bf117264f304ec51974c507668f
RayaRPARepo <mohamedabdelnabysaleh97@gmail.com>
2024-08-26 19:27:37 +0300

    First working version of embedding classification

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
 
 
 class Config:
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "sk-proj-swG6WDiaeGg7zbdnBv2YT3BlbkFJFS0GuPGIEViys7FqdLNl")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     EXCEL_FILE_PATH = "D:\\EmbeddingClassification\\MedicalCards_with_embeddings.csv"
 
 
